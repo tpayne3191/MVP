cab7a078b18479592dfe5b56e4e8897f49ca2edc
Ernest King <Ernest_C_King@Progressive.com>
2022-11-01 14:50:40 -0600

    Test skeleton added

---
+++ Tests/Capstone.DAL.Tests/CampaignRepositoryTests.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+namespace Capstone.DAL.Tests
+{
+    [TestFixture]
+    public class CampaignRepositoryTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void CreateTestSuccess()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ Tests/Capstone.DAL.Tests/CharacterWeaponRepositoryTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Capstone.DAL.Tests
+{
+    [TestFixture]
+    public class CharacterWeaponRepositoryTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void CreateTestSuccess()
+        {
+            Assert.Pass();
+        }
+    }
+}
