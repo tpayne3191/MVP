33c8d8a60767feb16c72bc3804d3cd8bda3c4d3a
David Dornbrack <ddornbr1@progressive.com>
2022-11-01 10:30:48 -0600

    Corrected Player class

--- Capstone.Core/Entities/Player.cs
+++ Capstone.Core/Entities/Player.cs
@@ -10,10 +10,10 @@ namespace Capstone.Core.Entities
         
         public string Name { get; set; }
 
-        public string Phone? { get; set; } 
+        public string Phone { get; set; } 
         
-        public string Email? { get; set; }
+        public string Email { get; set; }
 
-        public string City? { get; set; }
+        public string City { get; set; }
     }
 }
