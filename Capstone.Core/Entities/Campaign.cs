52bba5658a6e76bc7b53a272bb71d702608556b9
Terrell Payne <terrell_r_payne@progressive.com>
2022-11-01 11:46:14 -0400

    Campaign_entity

--- Capstone.Core/Entities/Campaign.cs
+++ Capstone.Core/Entities/Campaign.cs
@@ -6,5 +6,14 @@ namespace Capstone.Core.Entities
 {
     public class Campaign
     {
+        public int CampaignId { get; set; }
+        
+        public string Name { get; set; }
+
+        public DateTime DateStarted { get; set; }
+        
+        public DateTime DateEnded { get; set; }
+
+
     }
 }
