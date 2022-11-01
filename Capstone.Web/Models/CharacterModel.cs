8759742148ede08638983677ff4cb3dd9ae53932
Ernest King <Ernest_C_King@Progressive.com>
2022-11-01 14:13:43 -0600

    Model authentication requirements defined

--- Capstone.Web/Models/CharacterModel.cs
+++ Capstone.Web/Models/CharacterModel.cs
@@ -1,27 +1,51 @@
-﻿namespace Capstone.Web.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Capstone.Core.Entities;
+
+namespace Capstone.Web.Models
 {
     public class CharacterModel
     {
         // IDs
+        [Key]
         public int Id { get; set; }
+        [Required]
         public int PlayerId { get; set; }
+        [Required]
         public int CampaignId { get; set; }
 
         // About 
+        [Required]
         public int Level { get; set; }
+        [Required]
         public int ArmorClass { get; set; }
+        [Required]
         public int HitPoints { get; set; }
+        [Required]
+        [MaxLength(50)]
         public string Race { get; set; }
+        [Required]
+        [MaxLength(50)]
         public string Alignment { get; set; }
+        [Required]
+        [MaxLength(50)]
         public string Class { get; set; }
+        [Required]
+        [MaxLength(250)]
         public string Image { get; set; }
 
         // Stats
+        [Required]
         public int Strength { get; set; }
+        [Required]
         public int Dexterity { get; set; }
+        [Required]
         public int Constitution { get; set; }
+        [Required]
         public int Intelligence { get; set; }
+        [Required]
         public int Wisdom { get; set; }
+        [Required]
         public int Charisma { get; set; }
     }
 }
--- Capstone.Web/Models/PlayerModel.cs
+++ Capstone.Web/Models/PlayerModel.cs
@@ -1,11 +1,19 @@
-﻿namespace Capstone.Web.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Capstone.Web.Models
 {
     public class PlayerModel
     {
+        [Key]
         public int PlayerId { get; set; }
+        [Required]
+        [MaxLength(50)]
         public string Name { get; set; }
+        [MaxLength(50)]
         public string Phone { get; set; }
+        [MaxLength(50)]
         public string Email { get; set; }
+        [MaxLength(50)]
         public string City { get; set; }
     }
 }
