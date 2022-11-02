34a85ba74294eb68a9944f54e0f3f4157ac1c6c9
David Dornbrack <ddornbr1@progressive.com>
2022-11-02 08:32:23 -0600

    Updated All date data with DisplayFormat removing time

--- Capstone.Core/Entities/Campaign.cs
+++ Capstone.Core/Entities/Campaign.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Capstone.Core.Entities
@@ -8,7 +9,9 @@ namespace Capstone.Core.Entities
     {
         public int CampaignId { get; set; }
         public string Name { get; set; }
+        [DisplayFormat(DataFormatString = "{0:d}")]
         public DateTime DateStarted { get; set; }
+        [DisplayFormat(DataFormatString = "{0:d}")]
         public DateTime? DateEnded { get; set; }
     }
 }
