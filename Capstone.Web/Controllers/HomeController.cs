c1aab6081e0e99adcad904db927666477587e85f
A153728 <Zachary_W_Janouskovec@Progressive.com>
2022-11-02 16:33:37 -0400

    Added styling for HomeController and Index.cshtml

--- Capstone.Web/Controllers/HomeController.cs
+++ Capstone.Web/Controllers/HomeController.cs
@@ -18,20 +18,11 @@ namespace Capstone.Web.Controllers
             _logger = logger;
         }
 
+        [Route("/")]
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
