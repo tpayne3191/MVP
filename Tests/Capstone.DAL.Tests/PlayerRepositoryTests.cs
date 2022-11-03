f94ffc9f35cdc919a7fa49dfe55be656eed89ef2
Ernest King <Ernest_C_King@Progressive.com>
2022-11-03 10:17:02 -0600

    Setup, Teardown added to PlayerRepositoryTests

--- Tests/Capstone.DAL.Tests/PlayerRepositoryTests.cs
+++ Tests/Capstone.DAL.Tests/PlayerRepositoryTests.cs
@@ -1,6 +1,10 @@
-﻿using NUnit.Framework;
+﻿using Capstone.Core.Interfaces;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Text;
 
 namespace Capstone.DAL.Tests
@@ -8,9 +12,27 @@ namespace Capstone.DAL.Tests
     [TestFixture]
     public class PlayerRepositoryTests
     {
+        private DbConnection _connection;
+        private IPlayerRepository _playerRepository;
+
         [SetUp]
         public void Setup()
         {
+            _connection = new SqliteConnection("Filename=:memory:");
+            _connection.Open();
+            DbContextOptions<AppDbContext> options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseSqlite(_connection)
+                .Options;
+
+            AppDbContext dbContext = new AppDbContext(options);
+            if (dbContext.Database.EnsureCreated()) ;
+            _playerRepository = new PlayerRepository(dbContext);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _connection.Close();
         }
 
         [Test]
