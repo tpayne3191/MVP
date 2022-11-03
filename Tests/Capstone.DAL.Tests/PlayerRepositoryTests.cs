c15aa1e0894db92e03c191393c82fa86d43b79d5
Ernest King <Ernest_C_King@Progressive.com>
2022-11-03 10:36:00 -0600

    Player Create Test working successfully

--- Tests/Capstone.DAL.Tests/PlayerRepositoryTests.cs
+++ Tests/Capstone.DAL.Tests/PlayerRepositoryTests.cs
@@ -6,6 +6,8 @@ using System;
 using System.Collections.Generic;
 using System.Data.Common;
 using System.Text;
+using Capstone.Core;
+using Capstone.Core.Entities;
 
 namespace Capstone.DAL.Tests
 {
@@ -36,8 +38,80 @@ namespace Capstone.DAL.Tests
         }
 
         [Test]
-        public void CreateTestSuccess()
+        public void PlayerCreateTestSuccess()
         {
+            // Assign
+            Player player = new Player()
+            {
+                Name = "Ernest",
+                Email = "Ernest_C_King@Progressive.com",
+                City = "Colorado Springs",
+                Phone = "801-631-2160",
+                Characters = new List<Character>()
+            };
+            Result<Player> expectedResult = new Result<Player>()
+            {
+                Message = null,
+                Success = true,
+                Data = player
+            };
+            expectedResult.Data.Id = 1;
+
+            // Act
+            var actualResult = _playerRepository.Create(player);
+
+            // Assert
+            Assert.AreEqual(expectedResult.Success, actualResult.Success);
+            Assert.AreEqual(expectedResult.Message, actualResult.Message);
+            Assert.AreEqual(expectedResult.Data.Id, actualResult.Data.Id);
+            Assert.AreEqual(expectedResult.Data.City, actualResult.Data.City);
+            Assert.AreEqual(expectedResult.Data.Name, actualResult.Data.Name);
+            Assert.AreEqual(expectedResult.Data.Phone, actualResult.Data.Phone);
+            Assert.AreEqual(expectedResult.Data.Email, actualResult.Data.Email);
+        }
+
+        [Test]
+        public void PlayerReadAllSuccessfully()
+        {
+            // Assign
+
+            // Act
+
+            // Assert
+            Assert.Pass();
+        }
+
+        [Test]
+        public void PlayerReadByIdSuccessfully()
+        {
+
+            // Assign
+
+            // Act
+
+            // Assert
+            Assert.Pass();
+        }
+
+        [Test]
+        public void PlayerUpdateTestSuccessfully()
+        {
+            // Assign
+
+            // Act
+
+            // Assert
+            Assert.Pass();
+        }
+
+        [Test]
+        public void PlayerDeleteTestSuccessfully()
+        {
+            // Assign
+
+            // Act
+
+            // Assert
             Assert.Pass();
         }
     }
