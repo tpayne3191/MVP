f639a980c2e3f42ede863442ef64fc2e3abf095b
A153728 <Zachary_W_Janouskovec@Progressive.com>
2022-11-04 15:56:14 -0400

    Made change

--- Angular/CampaignManager/src/app/app-routing.module.ts
+++ Angular/CampaignManager/src/app/app-routing.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AppComponent } from './app.component';
 import { RouterModule, Routes } from '@angular/router';
 import { WeaponComponent } from './weapon/weapon.component';
 import { HomeComponent } from './home/home.component';
+import { CharacterComponent } from './character/character.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'weapon', component: WeaponComponent },
+  { path: 'character', component: CharacterComponent },
 ];
 
 @NgModule({
