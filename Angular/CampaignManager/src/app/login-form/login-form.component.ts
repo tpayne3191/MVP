21471e6933bd5ecbf37cb9253a8ba28d6137e854
Ernest King <Ernest_C_King@Progressive.com>
2022-11-08 09:31:48 -0700

    Working Angular Login with PlayerID set

--- Angular/CampaignManager/src/app/login-form/login-form.component.ts
+++ Angular/CampaignManager/src/app/login-form/login-form.component.ts
@@ -1,6 +1,9 @@
 import { Router } from '@angular/router';
-import { Component } from '@angular/core';
+import { PlayerService } from 'src/app/services/player.service';
+import { Component, OnInit } from '@angular/core';
 import { AuthServiceService } from '../services/auth-service.service';
+import { Observable } from 'rxjs';
+import Player from 'src/app/types/player.model';
 import User from '../types/user.model';
 
 @Component({
@@ -8,13 +11,24 @@ import User from '../types/user.model';
   templateUrl: './login-form.component.html',
   styleUrls: ['./login-form.component.css']
 })
-export class LoginFormComponent {
+export class LoginFormComponent implements OnInit {
   user: User = { userName: 'johncitizen', password: 'abc@123' } as User;
-  constructor(private authService: AuthServiceService, private router: Router) { }
+  playerId: number = {} as number;
+  players$: Observable<Player[]> = new Observable<Player[]>();
+
+  constructor(
+    private authService: AuthServiceService,
+    private router: Router,
+    private playerService: PlayerService
+    ) { }
+
+  ngOnInit(): void {
+    this.players$ = this.playerService.players$;
+  }
 
   handleSubmit() {
     try {
-      this.authService.loginPlayerId(this.user.userName, this.user.password, 1).subscribe(
+      this.authService.loginPlayerId(this.user.userName, this.user.password, this.playerId).subscribe(
         res => this.router.navigateByUrl('/'),
         err => {
           alert('username and/or password is incorrect');
